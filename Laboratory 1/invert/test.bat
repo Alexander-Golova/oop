ce494c4b28a5ccea7504cd29c1fe1e98b0716988
Alexander Kozlov <al.iv.kozlov@yandex.ru>
2016-10-08 23:08:10 +0300

    добавлена обработка отсутствия параметров

--- Laboratory 1/invert/test.bat
+++ Laboratory 1/invert/test.bat
@@ -1,13 +1,13 @@
 @ECHO on
 SET PROGRAM="%~1"
 
-echo Invert identity matrix
-CALL %PROGRAM% testInput\matrix1.txt > %TEMP%\correctMatrix1.txt
-IF ERRORLEVEL 1 GOTO errs
-FC %TEMP%\correctMatrix1.txt correctTest\correctMatrix1.txt
-IF ERRORLEVEL 1 GOTO errs
-DEL %TEMP%\correctMatrix1.txt
-ECHO Test on invertion the identity matrix SUCCEED
+REM No params
+CALL %PROGRAM% 2 > %TEMP%\err.txt
+IF NOT ERRORLEVEL 0 GOTO errs
+FC %TEMP%\err.txt correctTest\correctNoParams.txt
+IF ERRORLEVEL 1 GOTO err
+DEL %TEMP%\err.txt
+ECHO Test "no params" SUCCEED CEED
 
 ECHO ALL TESTS WERE SUCCEED
 EXIT 0
