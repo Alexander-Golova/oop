7980d46091317717c2e0116048a8e9bab1e4a447
Alexander Kozlov <al.iv.kozlov@yandex.ru>
2016-10-09 16:59:21 +0300

    тестирование единичной матрицы

--- Laboratory 1/invert/test.bat
+++ Laboratory 1/invert/test.bat
@@ -1,17 +1,14 @@
 @ECHO on
 SET PROGRAM="%~1"
 
-REM No params
-CALL %PROGRAM% 2 > %TEMP%\err.txt
-IF NOT ERRORLEVEL 0 GOTO errs
-FC %TEMP%\err.txt correctTest\correctNoParams.txt
+echo Invert identity matrix
+CALL %PROGRAM% testInput\matrix1.txt>%TEMP%\correctMatrix1.txt
 IF ERRORLEVEL 1 GOTO err
-DEL %TEMP%\err.txt
-ECHO Test "no params" SUCCEED CEED
+FC %TEMP%\correctMatrix1.txt correctTest\correctMatrix1.txt
+IF ERRORLEVEL 1 GOTO err
+DEL %TEMP%\correctMatrix1.txt
+ECHO Test on invertion the identity matrix SUCCEED
 
-ECHO ALL TESTS WERE SUCCEED
-EXIT 0
-
-:errs
+:err
 ECHO TEST FAILED
 EXIT 1
