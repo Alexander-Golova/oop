fdf58263c3a06aabd365f8929741e3b1d6e92d13
Alexander Kozlov <al.iv.kozlov@yandex.ru>
2016-10-10 11:44:34 +0300

    Добавлена функция подсчета количества соседей

--- Laboratory 1/live/live.cpp
+++ Laboratory 1/live/live.cpp
@@ -50,6 +50,84 @@ Matrix GetMatrixFromFile(std::ifstream & input, bool & error)
 	return initialMatrix;
 }
 
+size_t numberOfNeighbors(const Matrix & generation, const size_t & row, const size_t & col)
+{
+	size_t count = 0;
+	if (generation[row - 1][col - 1] == '#')
+	{
+		++count;
+	}
+	if (generation[row - 1][col] == '#')
+	{
+		++count;
+	}
+	if (generation[row - 1][col + 1] == '#')
+	{
+		++count;
+	}
+	if (generation[row][col - 1] == '#')
+	{
+		++count;
+	}
+	if (generation[row][col + 1] == '#')
+	{
+		++count;
+	}
+	if (generation[row + 1][col - 1] == '#')
+	{
+		++count;
+	}
+	if (generation[row + 1][col] == '#')
+	{
+		++count;
+	}
+	if (generation[row + 1][col + 1] == '#')
+	{
+		++count;
+	}
+	return count;
+}
+
+Matrix nextGeneration(const Matrix & generation)
+{
+	Matrix stateMatrix = GetNullMatrix();
+	for (size_t row = 0; row < dim; ++row)
+	{
+		for (size_t col = 0; col < dim; ++col)
+		{
+			if (generation[row][col] == '*')
+			{
+				stateMatrix[row][col] = '*';
+			}
+		}
+	}
+	for (size_t row = 1; row < dim - 1; ++row)
+	{
+		for (size_t col = 1; col < dim - 1; ++col)
+		{
+			if (generation[row][col] != '*')
+			{
+				if (numberOfNeighbors(generation, row, col) == 3)
+				{
+					stateMatrix[row][col] = '#';
+				}
+				if (numberOfNeighbors(generation, row, col) == 2 && generation[row][col] == '*')
+				{
+					stateMatrix[row][col] = '#';
+				}
+				{
+					stateMatrix[row][col] = '#';
+				}
+				if (numberOfNeighbors(generation, row, col) < 2 || numberOfNeighbors(generation, row, col) > 3)
+				{
+					stateMatrix[row][col] = ' ';
+				}
+			}			
+		}
+	}
+	return stateMatrix;
+}
+
 
 int main(int argc, char * argv[])
 {
@@ -68,20 +146,17 @@ int main(int argc, char * argv[])
 	}
 
 	Matrix firstGeneration;
-	Matrix nextGeneration;
+	Matrix secondGeneration;
 	bool error = false;
-
 	firstGeneration = GetMatrixFromFile(input, error);
-	PrintMatrix(firstGeneration);
-
 	if (error)
 	{
 		std::cerr << "The array is defined correctly" << std::endl;
 		return 1;
 	}
-
 	
+	secondGeneration = nextGeneration(firstGeneration);
 
-	//PrintMatrix(nextGeneration);
-	
+	PrintMatrix(secondGeneration);
+	return 0;
 }
