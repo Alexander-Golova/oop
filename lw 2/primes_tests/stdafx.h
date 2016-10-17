50279b1cf9fa297b8cb4817137b4019328e65c19
Alexander Kozlov <al.iv.kozlov@yandex.ru>
2016-10-17 17:24:40 +0300

    добавлены автотесты

---
+++ lw 2/primes_tests/stdafx.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "targetver.h"
+
+#include <set>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+
+#define BOOST_TEST_MODULE MyTest
+#define BOOST_TEST_INCLUDED 
+
+#include <boost/test/unit_test.hpp>
