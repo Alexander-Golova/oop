8edf211afc65ea2d87921b96ff67ed67d74f9494
Alexander Kozlov <al.iv.kozlov@yandex.ru>
2016-10-18 13:20:08 +0300

    добавлены автотесты

--- lw 2/FindAndReplace_tests/FindAndReplace_tests.cpp
+++ lw 2/FindAndReplace_tests/FindAndReplace_tests.cpp
@@ -2,10 +2,16 @@
 
 #include "../FindAndReplace/string_utils.h"
 
-BOOST_AUTO_TEST_SUITE(Test)
+BOOST_AUTO_TEST_SUITE(MyTest)
 
 BOOST_AUTO_TEST_CASE(Find_And_Replace)
 {
+	{
+		string subject = "123456789";
+		auto newString = FindAndReplace(subject, "1", "0");
+		const string correctString = "023456789";
+		BOOST_CHECK(newString == correctString);
+	}
 	{
 		string subject = "big paws, big black nose, stubby tail, and growl he goes";
 		auto newString = FindAndReplace(subject, "big", "small");
@@ -26,10 +32,11 @@ BOOST_AUTO_TEST_CASE(Find_And_Replace)
 	}
 	{
 		string subject = "1223344455566667777888899999...";
-		auto newString = FindAndReplace(subject, "6", "666");
-		const string correctString = "122334445556666666666667777888899999...";
+		auto newString = FindAndReplace(subject, "6", "0");
+		const string correctString = "1223344455500007777888899999...";
 		BOOST_CHECK(newString == correctString);
 	}
+
 }
 
 BOOST_AUTO_TEST_SUITE_END()
