af1a9b17b6543274442d5cd28574f3f534d0dc31
Alexander Kozlov <al.iv.kozlov@yandex.ru>
2016-10-18 12:34:40 +0300

    исправлена функция FindAndReplace

--- lw 2/FindAndReplace/main.cpp
+++ lw 2/FindAndReplace/main.cpp
@@ -4,34 +4,13 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc != 4)
+	if (!CheckArgumentCount(argc) && CheckReplacingString(argv[1]))
 	{
-		cerr << "Invalid arguments count" << endl
-			 << "Usage: replace <subject-string> <search-string> <replace-string>" << endl;
-		return 1;
-	}
-	string subjectString = argv[1];
-	if (subjectString.empty())
-	{
-		cerr << "Subject string must be not empty" << endl;
-		return 1;
-	}
-	string searchString = argv[2];
-	if (searchString.empty())
-	{
-		cerr << "Searching string must be not empty" << endl;
-		return 1;
-	}
-	string replaceString = argv[3];
-	if (replaceString.empty())
-	{
-		cerr << "Replace string must be not empty" << endl;
 		return 1;
 	}
 
-	string modifiedString = FindAndReplace(subjectString, searchString, replaceString);
+	string modifiedString = FindAndReplace(argv[1], argv[2], argv[3]);
 	cout << modifiedString << endl;
-
+	
 	return 0;
 }
-
--- lw 2/FindAndReplace/string_utils.h
+++ lw 2/FindAndReplace/string_utils.h
@@ -1,20 +1,50 @@
 #pragma once
 
 #include "stdafx.h"
-#include <boost/algorithm/searching/boyer_moore.hpp>
 
 using namespace std;
+
+
+bool CheckArgumentCount(const int argumentCount)
+{
+
+	if (argumentCount != 4)
+	{
+		cerr << "Invalid arguments count" << endl
+			 << "Usage: replace <subject-string> <search-string> <replace-string>" << endl;
+		return false;
+	}
+	return true;
+}
+
+bool CheckReplacingString(const string & str)
+{
+	if (str == "")
+	{
+		return true;
+	}
+	return false;
+}
 
 string FindAndReplace(string const & subject, string const & search, string  const & replace)
 {
 	string newString = "";
-	boost::algorithm::boyer_moore<std::string::const_iterator> search1(search.begin(), search.end());
-	if (search1(subject.begin(), subject.end()) != subject.end())
-		std::cout << "Found '" << search << "'  in '" << subject << "' (boyer-moore)" << std::endl;
-	else
-		std::cout << "Did NOT find '" << search << "'  in '" << subject << "' (boyer-moore)" << std::endl;
 
-
+	int leftPosition = 0;
+	int rightPosition = subject.find(search, 0);
+	
+	while (rightPosition != string::npos)
+	{
+		cout << rightPosition << endl;
+		newString += subject.substr(leftPosition, rightPosition);
+		newString += replace;
+		leftPosition = rightPosition + search.length();
+		rightPosition = subject.find(search, leftPosition);
+	}
+	if (rightPosition == string::npos)
+	{
+		newString += subject.substr(leftPosition, rightPosition);
+	}
 
 	return newString;
 }
