3b1ed695dcb31f3b33fbd4195e3f54e4c2cefa46
Alexander Kozlov <al.iv.kozlov@yandex.ru>
2016-10-20 11:42:48 +0300

    Refactoring

--- lw 2/vector/vector_utils.h
+++ lw 2/vector/vector_utils.h
@@ -4,36 +4,8 @@
 
 using namespace std;
 
-void PrintVector(ostream & output, const vector<double> & numbers)
-{
-	for (const auto & element : numbers)
-	{
-		output << fixed << setprecision(3) << element << " ";
-	}
-	output << endl;
-}
+void PrintVector(ostream & output, const vector<double> & numbers);
 
-vector<double> GetNumbers(istream & input)
-{
-	return{ istream_iterator<double>(input), istream_iterator<double>() };
-}
+vector<double> GetNumbers(istream & input);
 
-void VectorÑonversion(vector<double> & numbers)
-{
-	if (numbers.empty())
-	{
-		return;
-	}
-	
-	double min = *min_element(numbers.begin(), numbers.end());
-	if (min == 0)
-	{
-		return;
-	}
-
-	double multiplier = *max_element(numbers.begin(), numbers.end()) / min;
-	transform(numbers.begin(), numbers.end(), numbers.begin(), [=](double element) {
-		return element * multiplier;
-	});
-
-}
+void VectorÑonversion(vector<double> & numbers);
