0ac744b9a136ecae33820980306b927d0e70f58f
Alexander Kozlov <al.iv.kozlov@yandex.ru>
2016-10-20 09:59:58 +0300

    Refactoring

---
+++ lw 2/FindAndReplace/string_utils.cpp
@@ -0,0 +1,47 @@
+#include "stdafx.h"
+
+#include "string_utils.h"
+
+bool CheckArgumentCount(const int argumentCount)
+{
+
+	if (argumentCount != 4)
+	{
+		cerr << "Invalid arguments count" << endl
+			<< "Usage: replace <subject-string> <search-string> <replace-string>" << endl;
+		return false;
+	}
+	return true;
+}
+
+bool CheckReplacingString(const string & str)
+{
+	if (str == "")
+	{
+		return true;
+	}
+	return false;
+}
+
+string FindAndReplace(string const & subject, string const & search, string  const & replace)
+{
+	string newString = "";
+
+	int leftPosition = 0;
+	int rightPosition = 0;
+
+	while (rightPosition != string::npos)
+	{
+		rightPosition = subject.find(search, leftPosition);
+		if (rightPosition != string::npos)
+		{
+			newString += subject.substr(leftPosition, rightPosition - leftPosition);
+			newString += replace;
+			leftPosition = rightPosition + search.length();
+		}
+	}
+
+	newString += subject.substr(leftPosition, subject.length());
+
+	return newString;
+}
