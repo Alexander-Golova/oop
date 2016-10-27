33dfc30a7122ec68dd412f1007e2c06eff599705
Alexander Kozlov <al.iv.kozlov@yandex.ru>
2016-10-27 16:19:39 +0300

    Refactoring

--- lw 2/translator_test/translator_test.cpp
+++ lw 2/translator_test/translator_test.cpp
@@ -1,13 +1,8 @@
 #include "stdafx.h"
 
-#include "../translator/translator_utils.cpp"
+#include "../translator/translator_utils.h"
 
 using namespace std;
-
-bool VectorsAreEqual(vector<string> const& x, vector<string> const& y)
-{
-	return x == y;
-}
 
 BOOST_AUTO_TEST_SUITE(MyTest)
 
@@ -33,20 +28,29 @@ BOOST_AUTO_TEST_CASE(Adding_and_searching_for_words_in_the_dictionary)
 		Dictionary dictionary;
 		AddWordToDictionaryy("cat", "кот", dictionary);
 		auto words = FindWordInDictionary("cat", dictionary);
-		BOOST_CHECK(VectorsAreEqual(words, { "кот" }));
+		const vector<string> expectedResult = {
+			"кот"
+		};
+ 		BOOST_CHECK(words == expectedResult);
 	}
 	{
 		Dictionary dictionary;
 		AddWordToDictionaryy("cat", "кот", dictionary);
 		auto words = FindWordInDictionary("кот", dictionary);
-		BOOST_CHECK(VectorsAreEqual(words, { "cat" }));
+		const vector<string> expectedResult = {
+			"cat"
+		};
+		BOOST_CHECK(words == expectedResult);
 	}
 	{
 		Dictionary dictionary;
 		AddWordToDictionaryy("кот", "cat", dictionary);
 		AddWordToDictionaryy("кошка", "cat", dictionary);
 		auto words = FindWordInDictionary("cat", dictionary);
-		BOOST_CHECK(VectorsAreEqual(words, { "кот", "кошка" }));
+		const vector<string> expectedResult = {
+			"кот", "кошка"
+		};
+		BOOST_CHECK(words == expectedResult);
 	}
 }
 
