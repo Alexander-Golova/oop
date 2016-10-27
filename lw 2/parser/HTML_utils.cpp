18a6fd6255c840bc92baf183e64347df7332c978
Alexander Kozlov <al.iv.kozlov@yandex.ru>
2016-10-27 16:12:08 +0300

    Refactoring

--- lw 2/parser/HTML_utils.cpp
+++ lw 2/parser/HTML_utils.cpp
@@ -53,41 +53,34 @@ int GetPort(const Protocol & protocol)
 bool ParseURL(string const & url, Protocol &  protocol, int & port, string & host, string & document)
 {
 	regex regexStr1("(http|https|ftp|HTTP|HTTPS|FTP)://([0-9a-z\.-]+):([0-9]+)/([^[:s:]]+){0,}");
-	smatch result;
-	if (regex_match(url, result, regexStr1))
-	{
-		string regProt(result[1].first, result[1].second);
-		if (!GetProtocol(regProt, protocol))
-		{
-			return false;
-		}		
-		string regPort(result[3].first, result[3].second);
-		port = atoi(regPort.c_str());
-		if (port < 1 || port > 65535)
-		{
-			return false;
-		}		
-		string regHost(result[2].first, result[2].second);
-		host = regHost;		
-		string regDoc(result[4].first, result[4].second);
-		document = regDoc;
-		return true;
-	}
-
 	regex regexStr2("(http|https|ftp|HTTP|HTTPS|FTP)://([0-9a-z\.-]+)/([^[:s:]]+){0,}");
+	smatch result;
 
-	if (regex_match(url, result, regexStr2))
-	{
-		string regProt(result[1].first, result[1].second);
-		if (!GetProtocol(regProt, protocol))
+	if (regex_match(url, result, regexStr1))
+	{		
+		if (!GetProtocol(string(result[1].first, result[1].second), protocol))
 		{
 			return false;
 		}
-		port = GetPort(protocol);		
-		string regHost(result[2].first, result[2].second);
-		host = regHost;
-		string regDoc(result[3].first, result[3].second);
-		document = regDoc;
+		port = atoi(string(result[3].first, result[3].second).c_str());
+		if (port < 1 || port > 65535)
+		{
+			return false;
+		}
+		host = string(result[2].first, result[2].second);
+		document = string(result[4].first, result[4].second);
+		return true;
+	}
+
+	if (regex_match(url, result, regexStr2))
+	{
+		if (!GetProtocol(string(result[1].first, result[1].second), protocol))
+		{
+			return false;
+		}
+		port = GetPort(protocol);
+		host = string(result[2].first, result[2].second);
+		document = string(result[3].first, result[3].second);
 		return true;
 	}
 	return false;
