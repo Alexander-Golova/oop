03c0b84d3ec8a4d2398732ff5b00eafa0f4bfa05
Alexander Kozlov <al.iv.kozlov@yandex.ru>
2016-11-01 21:43:49 +0300

    Добавлены тесты проверяющие выключенный автомобиль

--- lw3/car_tests/car_tests.cpp
+++ lw3/car_tests/car_tests.cpp
@@ -16,9 +16,24 @@ BOOST_FIXTURE_TEST_SUITE(Car, CarFixture)
 		BOOST_CHECK(!car.IsEngineTurnOn());
 	}
 	// находится на нейтральной передаче
-	BOOST_AUTO_TEST_CASE(on_the_engine_is_switched_off_is_in_neutral)
+	BOOST_AUTO_TEST_CASE(is_on_neutral_gear_by_default)
 	{
 		BOOST_CHECK(car.GetGear() == Gear::Neutral);
+	}
+	// имеет нулевую скорость
+	BOOST_AUTO_TEST_CASE(it_has_zero_speed)
+	{
+		BOOST_CHECK(car.GetSpeed() == 0);
+	}
+	// нельзя выбрать передачу в выключенном состоянии
+	BOOST_AUTO_TEST_CASE(can_not_select_the_transmission_off)
+	{
+		BOOST_CHECK(!car.SetSpeed(1));
+	}
+	// у выключенного автомобиля направление движения нет (стоим на месте)
+	BOOST_AUTO_TEST_CASE(auto_shut_down_at_the_direction_of_motion_is_not)
+	{
+		BOOST_CHECK(car.GetDirection() == Direction::Stop);
 	}
 
 BOOST_AUTO_TEST_SUITE_END()
