e67c8e0fdf4342fd45549a74d04a6619005598d8
Alexander Kozlov <al.iv.kozlov@yandex.ru>
2016-11-05 12:16:18 +0300

    Вывод автотестов оформлен в виде лесенки

--- lw 2/translator_test/stdafx.h
+++ lw 2/translator_test/stdafx.h
@@ -2,20 +2,8 @@
 
 #include "targetver.h"
 
-#include <stdio.h>
-#include <tchar.h>
-#include <map>
-#include <string>
-#include <iostream>
-#include <fstream>
-#include <strstream>
-#include <locale>
-#include <vector>
-#include <iterator>
-#include <algorithm>
-#include <Windows.h>
-
-#define BOOST_TEST_MODULE MyTest
-#define BOOST_TEST_INCLUDED 
+#define BOOST_TEST_INCLUDED
 
 #include <boost/test/unit_test.hpp>
+
+#include <boost/algorithm/string/replace.hpp>
