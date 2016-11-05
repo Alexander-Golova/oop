da3959a293dc4cba0433c1f6d4d65639962d9a8b
Alexander Kozlov <al.iv.kozlov@yandex.ru>
2016-11-05 11:40:52 +0300

    В тестах использоны человекочитаемые данные

--- lw 2/FindAndReplace_tests/FindAndReplace_tests.cpp
+++ lw 2/FindAndReplace_tests/FindAndReplace_tests.cpp
@@ -1,49 +1,52 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
 
 #include "../FindAndReplace/string_utils.h"
 
 using namespace std;
 
-BOOST_AUTO_TEST_SUITE(MyTest)
+// функция Find_And_Replace
+BOOST_AUTO_TEST_SUITE(Find_And_Replace_function)
 
-BOOST_AUTO_TEST_CASE(replacing_empty_string_should_return_subject)
-{
-	BOOST_CHECK_EQUAL(FindAndReplace("subj", "", "something"), "subj");
-}
-
-BOOST_AUTO_TEST_CASE(Find_And_Replace)
-{
+	// не производит поиск в пустой строке
+	BOOST_AUTO_TEST_CASE(it_does_not_search_a_blank_line)
 	{
-		string subject = "123456789";
-		auto newString = FindAndReplace(subject, "1", "");
-		const string correctString = "23456789";
-		BOOST_CHECK(newString == correctString);
-	}
-	{
-		string subject = "123456789";
-		auto newString = FindAndReplace(subject, "1", "0");
-		const string correctString = "023456789";
-		BOOST_CHECK(newString == correctString);
-	}
-	{
-		string subject = "123456789";
-		auto newString = FindAndReplace(subject, "1", "0000");
-		const string correctString = "000023456789";
-		BOOST_CHECK(newString == correctString);
-	}
-	{
-		string subject = "123456789";
-		auto newString = FindAndReplace(subject, "0", "000");
-		const string correctString = "123456789";
-		BOOST_CHECK(newString == correctString);
-	}
-	{
-		string subject = "1234526789";
-		auto newString = FindAndReplace(subject, "2", "000");
-		const string correctString = "10003450006789";
-		BOOST_CHECK(newString == correctString);
+		BOOST_CHECK_EQUAL(FindAndReplace("", "replace", "something"), "");
 	}
 
-}
+	// не ищет пустую строку
+	BOOST_AUTO_TEST_CASE(replacing_empty_string_should_return_subject)
+	{
+		BOOST_CHECK_EQUAL(FindAndReplace("subj", "", "something"), "subj");
+	}
+
+	// не меняет исходную строку, если строка поиска не найдена 
+	BOOST_AUTO_TEST_CASE(it_does_not_change_the_original_string_if_the_search_string_is_not_found)
+	{
+		BOOST_CHECK_EQUAL(FindAndReplace("subject", "search", "replace"), "subject");
+	}
+
+	// заменяет одно вхождение строки поиска
+	BOOST_AUTO_TEST_CASE(replaces_the_entry_search_for_replace)
+	{
+		string subject = "I use different search engine.";
+		const string correctString = "I use different replace engine.";
+		BOOST_CHECK_EQUAL(FindAndReplace(subject, "search", "replace"), correctString);
+	}
+
+	// заменяет все вхождение строки поиска на слово меньшей длины
+	BOOST_AUTO_TEST_CASE(it_replaces_all_occurrences_of_a_search_string_in_the_word_of_shorter_length)
+	{
+		string subject = "Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo";
+		const string correctString = "Buffalo bison Buffalo bison bison bison Buffalo bison";
+		BOOST_CHECK_EQUAL(FindAndReplace(subject, "buffalo", "bison"), correctString);
+	}
+
+	// заменяет все вхождение строки поиска на слово большей длины
+	BOOST_AUTO_TEST_CASE(it_replaces_all_occurrences_of_the_search_string_word_for_a_greater_length)
+	{
+		string subject = "Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo";
+		const string correctString = "Buffalo amphibian Buffalo amphibian amphibian amphibian Buffalo amphibian";
+		BOOST_CHECK_EQUAL(FindAndReplace(subject, "buffalo", "amphibian"), correctString);
+	}
 
 BOOST_AUTO_TEST_SUITE_END()
