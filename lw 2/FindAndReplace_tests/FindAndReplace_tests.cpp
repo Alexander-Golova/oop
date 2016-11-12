6011ed9e9ec987115f2d12876c075a40b547932f
Alexey Malov <alexey.malov@cpslabs.net>
2016-11-12 11:31:16 +0300

    Подкорректировал имена тестов

--- lw 2/FindAndReplace_tests/FindAndReplace_tests.cpp
+++ lw 2/FindAndReplace_tests/FindAndReplace_tests.cpp
@@ -8,19 +8,19 @@ using namespace std;
 BOOST_AUTO_TEST_SUITE(Find_And_Replace_function)
 
 	// не производит поиск в пустой строке
-	BOOST_AUTO_TEST_CASE(it_does_not_search_a_blank_line)
+	BOOST_AUTO_TEST_CASE(does_not_search_in_empty_string)
 	{
 		BOOST_CHECK_EQUAL(FindAndReplace("", "replace", "something"), "");
 	}
 
 	// не ищет пустую строку
-	BOOST_AUTO_TEST_CASE(replacing_empty_string_should_return_subject)
+	BOOST_AUTO_TEST_CASE(replacement_of_empty_string_should_return_subject)
 	{
 		BOOST_CHECK_EQUAL(FindAndReplace("subj", "", "something"), "subj");
 	}
 
 	// не меняет исходную строку, если строка поиска не найдена 
-	BOOST_AUTO_TEST_CASE(it_does_not_change_the_original_string_if_the_search_string_is_not_found)
+	BOOST_AUTO_TEST_CASE(does_not_change_the_original_string_if_the_search_string_is_not_found)
 	{
 		BOOST_CHECK_EQUAL(FindAndReplace("subject", "search", "replace"), "subject");
 	}
@@ -34,7 +34,7 @@ BOOST_AUTO_TEST_SUITE(Find_And_Replace_function)
 	}
 
 	// заменяет все вхождение строки поиска на слово меньшей длины
-	BOOST_AUTO_TEST_CASE(it_replaces_all_occurrences_of_a_search_string_in_the_word_of_shorter_length)
+	BOOST_AUTO_TEST_CASE(replaces_all_occurrences_of_a_search_string_in_the_word_of_shorter_length)
 	{
 		string subject = "Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo";
 		const string correctString = "Buffalo bison Buffalo bison bison bison Buffalo bison";
@@ -42,7 +42,7 @@ BOOST_AUTO_TEST_SUITE(Find_And_Replace_function)
 	}
 
 	// заменяет все вхождение строки поиска на слово большей длины
-	BOOST_AUTO_TEST_CASE(it_replaces_all_occurrences_of_the_search_string_word_for_a_greater_length)
+	BOOST_AUTO_TEST_CASE(replaces_all_occurrences_of_the_search_string_word_for_a_greater_length)
 	{
 		string subject = "Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo";
 		const string correctString = "Buffalo amphibian Buffalo amphibian amphibian amphibian Buffalo amphibian";
