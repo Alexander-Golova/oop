ee2b6a3f6309b4c708d9087b94c8e8b8fe1a3a13
Alexander Kozlov <al.iv.kozlov@yandex.ru>
2016-11-14 17:56:52 +0300

    Добавлены функции, возвращающих цвета границы и заливки

--- lw4/GeometricShapes/IShape.h
+++ lw4/GeometricShapes/IShape.h
@@ -10,8 +10,8 @@ public:
 
 	virtual double GetArea() const = 0;
 	virtual double GetPerimeter() const = 0;
-	/*virtual std::string ToString() const = 0;
-	virtual std::string GetOutlineColor() const = 0;*/
+	/*virtual std::string ToString() const = 0;*/
+	virtual std::string GetOutlineColor() const = 0;
 
 private:
 };
@@ -23,7 +23,7 @@ public:
 	ISolidShape() = default;
 	virtual ~ISolidShape() = default;
 
-	/*virtual std::string GetFillColor() const = 0;*/
+	virtual std::string GetFillColor() const = 0;
 
 private:
 };
