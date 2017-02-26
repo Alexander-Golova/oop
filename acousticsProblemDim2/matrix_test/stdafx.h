aae102536afe0819e6e8cf3b3011d3e3936a0054
Alexander Kozlov <al.iv.kozlov@yandex.ru>
2017-02-27 00:03:54 +0300

    Начало тестирования функций работы с матрицами

---
+++ acousticsProblemDim2/matrix_test/stdafx.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "targetver.h"
+
+#define BOOST_TEST_MODULE MyTest
+#define BOOST_TEST_INCLUDED 
+
+#include <boost/test/unit_test.hpp>
+
+#include <complex>
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
