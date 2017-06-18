7df6419c889009000ab1948e2a6dd5094c838330
Alexander Kozlov <al.iv.kozlov@yandex.ru>
2017-06-18 14:10:24 +0300

    Восьмая лабораторная работа по ОС. Создание клиента для работы со службой

---
+++ OS/lw8_Client/main.cpp
@@ -0,0 +1,59 @@
+#include "stdafx.h"
+
+using namespace std;
+
+const DWORD BUFFER_SIZE = 2097152; // 2 Mb
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << endl << "File system emulator client start..." << endl;
+
+	HANDLE hPipe = CreateFile(L"\\\\.\\pipe\\fse_pipe", GENERIC_READ | GENERIC_WRITE,
+		FILE_SHARE_READ | FILE_SHARE_WRITE, NULL,
+		OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	if (hPipe == INVALID_HANDLE_VALUE)
+	{
+		cout << "Failed to connect to named pipe" << endl;
+		return 1;
+	}
+
+	string command;
+	static char buffer[BUFFER_SIZE];
+	DWORD numBytes = 0;
+
+	ReadFile(hPipe, buffer, BUFFER_SIZE, &numBytes, NULL);
+
+	string startMsg(buffer, numBytes);
+	cout << endl << startMsg;
+
+	cout << endl << "> ";
+
+	while (getline(cin, command))
+	{
+		if (!WriteFile(hPipe, command.c_str(), command.length(), &numBytes, NULL))
+		{
+			cout << "Failed to write data in the pipe" << endl;
+		}
+
+		if (!ReadFile(hPipe, buffer, BUFFER_SIZE, &numBytes, NULL))
+		{
+			cout << "Failed to read data from the pipe" << endl;
+			continue;
+		}
+
+		string msg(buffer, numBytes);
+		cout << endl << "service> " << msg;
+
+		if (msg == "disconnect")
+		{
+			break;
+		}
+
+		cout << endl << "> ";
+	}
+
+	CloseHandle(hPipe);
+
+	return 0;
+}
