fd0ffa8f5f10684077fba9f30243455fe8c2f9fa
Alexander Kozlov <al.iv.kozlov@yandex.ru>
2020-02-05 22:39:25 +0300

    Добавлен проект live

---
+++ lw1/live/pch.h
@@ -0,0 +1,13 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include <array>
+#include <cctype>
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#endif //PCH_H
