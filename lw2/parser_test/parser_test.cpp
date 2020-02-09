7119500545caf7507a408effcbcf0d376adf8769
Alexander Kozlov <al.iv.kozlov@yandex.ru>
2020-02-09 09:41:31 +0300

    Создан успешный автотест

--- lw2/parser_test/parser_test.cpp
+++ lw2/parser_test/parser_test.cpp
@@ -1,7 +1,23 @@
 #include "pch.h"
-#include <iostream>
 
-int main()
+#include "../parser/HTML_utils.h"
+
+using namespace std;
+
+// ќжидаем успех команды
+void ExpectCorrectURL(const string& url, Protocol expectedProtocol, const string& expectedHost, int expectedPort, const string& exepectedDocument)
 {
-    std::cout << "Hello World!\n"; 
+	string host, document;
+	Protocol protocol;
+	int port;
+	REQUIRE(ParseURL(url, protocol, port, host, document));
+	CHECK(tie(protocol, port, host, document) == tie(expectedProtocol, expectedPort, expectedHost, exepectedDocument));
+}
+
+// ‘ункци€ ParseURL извлекает протокол, хост, порт и документ из URL-а
+TEST_CASE("Parse URL function extracts the protocol host port document from URL")
+{
+	ExpectCorrectURL("http://hostname:87/doc.txt", Protocol::HTTP, "hostname", 87, "doc.txt");
+	ExpectCorrectURL("ftp://hostname:87/doc.txt", Protocol::FTP, "hostname", 87, "doc.txt");
+	ExpectCorrectURL("https://hostname:87/doc.txt", Protocol::HTTPS, "hostname", 87, "doc.txt");
 }
--- lw2/parser_test/pch.h
+++ lw2/parser_test/pch.h
@@ -1,6 +1,14 @@
 #ifndef PCH_H
 #define PCH_H
 
+#include "../../catch/catch.hpp"
 
+#include <cstdlib>
+#include <iostream>
+#include <regex>
+#include <stdio.h>
+#include <string>
+#include <tchar.h>
+#include <tuple>
 
 #endif //PCH_H
