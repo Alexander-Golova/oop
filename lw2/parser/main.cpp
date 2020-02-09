e05fd3b9f40b727a52e8d8235b86ea0e1db23ead
Alexander Kozlov <al.iv.kozlov@yandex.ru>
2020-02-09 09:28:11 +0300

    Написаны функции программы parser

--- lw2/parser/main.cpp
+++ lw2/parser/main.cpp
@@ -1,7 +1,30 @@
 #include "pch.h"
-#include <iostream>
+
+#include "HTML_utils.h"
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	string url;
+	Protocol protocol;
+	int port;
+	string host;
+	string document;
+
+	while (getline(cin, url))
+	{
+		if (ParseURL(url, protocol, port, host, document))
+		{
+			cout << url << endl;
+			cout << "HOST: " << host << endl;
+			cout << "PORT: " << port << endl;
+			cout << "DOC: " << document << endl;
+		}
+		else
+		{
+			cout << "The specified URL is invalid" << endl;
+		}
+	}
+	return 0;
 }
--- lw2/parser/pch.h
+++ lw2/parser/pch.h
@@ -1,6 +1,12 @@
 #ifndef PCH_H
 #define PCH_H
 
+#include <cstdlib>
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <tchar.h>
 
+#include <regex>
 
 #endif //PCH_H
