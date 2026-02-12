e9711cf35e456b8eb43fd540e3ddec42e9dfc0b5
Alexander Kozlov <al.iv.kozlov@yandex.ru>
2026-02-12 23:44:45 +0300

    Delete lw 2 directory

--- lw 2/FindAndReplace/string_utils.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "stdafx.h"
-
-bool CheckArgumentCount(const int argumentCount);
-
-std::string FindAndReplace(const std::string & subject, const std::string & search, const std::string & replace);
--- lw 2/primes/stdafx.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "targetver.h"
-
-#include <set>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-#include <cctype>
