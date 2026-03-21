ae6223e43a7797cc3e6dd6b4bd930444c0eb3ac8
Alexander Kozlov <al.iv.kozlov@yandex.ru>
2026-03-21 14:28:09 +0300

    Собран каркас класса Body

---
+++ Inheritance/Bodies/Body.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+#include <sstream>
+#include <iomanip>
+
+class CBody
+{
+public:
+	CBody(const std::string& type, double density);
+
+	double GetDensity() const;
+	virtual double GetVolume() const = 0;
+	double GetMass() const;
+	std::string ToString() const;
+
+	virtual ~CBody() = default;
+
+private:	
+	virtual void AppendProperties(std::ostream& strm) const = 0;
+	double m_density;
+	std::string m_type;
+};
